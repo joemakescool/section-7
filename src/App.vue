27b159bdbabb20b268075b2b62e14ca8f27a79e6
joebarker <joebarker@utah.gov>
2019-11-06 16:32:44 -0700

    main

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <app-server-status></app-server-status>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+  },
+
+  data: () => ({
+    serverStatus: 'Critical!'
+  }),
 }
 </script>
 
